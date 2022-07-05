d4724edad58c056c5a9c721a677a16ed69fef415
kleveck <kcleveque@gmail.com>
2022-07-05 03:40:47 -0500

    Hard coded message to education page

--- pages/education.js
+++ pages/education.js
@@ -7,6 +7,14 @@ const education = () => {
       <div className={eduStyles.hero}>
         <h1 className={eduStyles.linear__w}>Education</h1>
       </div>
+      <article className="container relative h-44">
+        <h1 className="text-3xl text-white font-stencil text-center">
+          No Posts to show at this time
+        </h1>
+        <h2 className="text-2xl text-white text-center font-stencil">
+          Please check back later
+        </h2>
+      </article>
     </>
   );
 };
